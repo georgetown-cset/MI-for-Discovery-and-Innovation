7e70745c5b0e2905c18aa7e4616e4a63b7c67297
atoney-CSET <62558612+atoney-CSET@users.noreply.github.com>
2021-02-24 15:55:10 -0500

    updated comments

--- ai_rc_growth_queries.sql
+++ ai_rc_growth_queries.sql
@@ -5,12 +5,14 @@ GROUP BY level0_mag
 ORDER BY cluster_count DESC
 
 --count how many research clusters by level 0 MAG category where level 0 MAG category is not Computer Science and 3yr average growth is greater than or equal to 20%
+--generates AI for Scientific Discovery_20%growth.csv
 SELECT COUNT(cluster_id) as cluster_count, level0_mag FROM `gcp-cset-projects.science_map.dc5_stat_clust_proc_201021`
 WHERE NOT level0_mag = "computer science" AND ai_pred >= 0.1 AND growth_3yr >= 0.2
 GROUP BY level0_mag
 ORDER BY cluster_count DESC
 
 --count how many research clusters by level 0 MAG category where level 0 MAG category is not Computer Science and 3yr average growth is greater than or equal to 100%
+--generates AI for Scientific Discovery_100%growth.csv
 SELECT COUNT(cluster_id) as cluster_count, level0_mag FROM `gcp-cset-projects.science_map.dc5_stat_clust_proc_201021`
 WHERE NOT level0_mag = "computer science" AND ai_pred >= 0.1 AND growth_3yr >= 1
 GROUP BY level0_mag
